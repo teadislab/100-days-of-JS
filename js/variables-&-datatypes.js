d291053fd6e6dd13185a7affc4fd72a2edf6fff5
Aditya <Aditya.patil0404@gmail.com>
2022-11-06 20:04:50 +0530

    Added Variables-&-datatypes

---
+++ js/variables-&-datatypes.js
@@ -0,0 +1,21 @@
+// There are 4 ways to declare variables
+//using var, let, const and nothing
+var x = 5;
+var y = 6;
+var z = x + y;
+// var was used in the initial versions, this was a confusing one.
+// The biggest drawback was redeclare.
+var x=10;
+//as demonstrated in above line
+//let is mostly used as a versitile keyword
+let a = 1;
+let b = 1;
+let c = a + b;
+
+//Variable Types
+//There is no need to specifiy Variable types in JS
+//For intigers, floting points
+let myAge = 22;
+let pi = 3.14;
+//For String
+let name = 'Aditya'
