898c3203b5d74953b41398d602f5ec29464b9dd2
Aditya <Aditya.patil0404@gmail.com>
2022-11-10 23:12:42 +0530

    Added Numbers Info

---
+++ js/numbers-string-operator.js
@@ -0,0 +1,8 @@
+//Types of Number in JS
+//Integers are floating-point numbers without a fraction. They can either be positive or negative, e.g. 10, 400, or -5.
+//Floating point numbers (floats) have decimal points and decimal places, for example 12.5, and 56.7786543.
+//Doubles are a specific type of floating point number that have greater precision than standard floating point numbers (meaning that they are accurate to a greater number of decimal places).
+
+const myInt = 5;
+const myFloat = 6.667;
+
