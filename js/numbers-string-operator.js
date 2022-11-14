79fa91fc87ab942f51961c988e5fcedc8aee4cd1
Aditya <Aditya.patil0404@gmail.com>
2022-11-14 23:33:05 +0530

    Added Stinge Info

--- js/numbers-string-operator.js
+++ js/numbers-string-operator.js
@@ -7,3 +7,16 @@ const myInt = 5;
 const myFloat = 6.667;
 
 //String
+//declearing a String
+const string = "The revolution will not be televised.";
+//there are two types of wraping in JS
+//Single Quotes Vs. double Quotes
+const sgl = 'Single quotes.';
+const dbl = "Double quotes";
+//he browser will think the string has not been closed because the other type of quote you are not using to contain your strings can appear in the string. For example, both of these are okay:
+const sglDbl = 'Would you eat a "fish supper"?';
+const dblSgl = "I'm feeling blue.";
+
+//Escaping characters in a string
+//add more info on this
+const bigmouth = 'I\'ve got no right to take my place…';
