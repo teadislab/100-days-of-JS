43184f7faba4d12f046b22146923d9ba5af48054
Aditya <Aditya.patil0404@gmail.com>
2022-11-15 20:35:42 +0530

    Updated w/String Operation

--- js/numbers-string-operator.js
+++ js/numbers-string-operator.js
@@ -2,7 +2,6 @@
 //Integers are floating-point numbers without a fraction. They can either be positive or negative, e.g. 10, 400, or -5.
 //Floating point numbers (floats) have decimal points and decimal places, for example 12.5, and 56.7786543.
 //Doubles are a specific type of floating point number that have greater precision than standard floating point numbers (meaning that they are accurate to a greater number of decimal places).
-
 const myInt = 5;
 const myFloat = 6.667;
 
@@ -13,10 +12,18 @@ const string = "The revolution will not be televised.";
 //Single Quotes Vs. double Quotes
 const sgl = 'Single quotes.';
 const dbl = "Double quotes";
-//he browser will think the string has not been closed because the other type of quote you are not using to contain your strings can appear in the string. For example, both of these are okay:
+//The browser will think the string has not been closed because the other type of quote you are not using to contain your strings can appear in the string. 
+//For example, both of these are okay:
 const sglDbl = 'Would you eat a "fish supper"?';
 const dblSgl = "I'm feeling blue.";
 
 //Escaping characters in a string
-//add more info on this
-const bigmouth = 'I\'ve got no right to take my place…';
+//Escaping characters means that we do something to them to make sure they are recognized as text, not part of the code. 
+//In JavaScript, we do this by putting a backslash just before the character. 
+const bigmouth = 'I\'ve got no right to take my place…'; 
+
+//Concatenating String
+//Concatenate just means "join together". 
+//To join together strings in JavaScript you can use a different type of string, called a template literal.
+const name = "Chris";
+const greeting = `Hello, ${name}`
